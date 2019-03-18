8cef8fc05db164fff8f5cf6119e51cdbe1d17690
Jeffer1894 <jefer1894@hotmail.com>
2019-03-18 16:42:36 -0500

    Add files via upload

--- poo basico/Mexflix/Index.php
+++ poo basico/Mexflix/Index.php
@@ -1,4 +1,6 @@
 <?php
-$route_index= isset ($_GET['r'])  ? $_GET['r'] /*Si la variable 'r' viene definida asigna el valor de 'r'*/: 'home' /*en caso contrario que no venga definida se asigna valor 'home'*/; /*'r' variable definida en .htaccess en la raiz*/
+require_once('./controllers/Autoload.php');//Autoload.php se encarga de llamar todos los archivos de esta aplicación 
+$autoload=new Autoload(); // Objeto que inicializa Autoload
+$route_index= isset ($_GET['r'])  ? $_GET['r']: 'home'; /*Si la variable 'r' viene definida asigna el valor de 'r'*/ /*en caso contrario que no venga definida se asigna valor 'home'*//*'r' variable definida en .htaccess en la raiz*/
 /*Isset: funciona para conocer si una variable esta definida o no*/
 $mexflix= new Router($route_index);
