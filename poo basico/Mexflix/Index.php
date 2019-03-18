c3ef187d212bced08f5e353a80835ab8f95b04d1
Jeffer1894 <jefer1894@hotmail.com>
2019-03-18 10:00:47 -0500

    Add files via upload

--- poo basico/Mexflix/Index.php
+++ poo basico/Mexflix/Index.php
@@ -1,1 +1,4 @@
-<h1>Bienvenido a Mexflix </h1>
+<?php
+$route_index= isset ($_GET['r'])  ? $_GET['r'] /*Si la variable 'r' viene definida asigna el valor de 'r'*/: 'home' /*en caso contrario que no venga definida se asigna valor 'home'*/; /*'r' variable definida en .htaccess en la raiz*/
+/*Isset: funciona para conocer si una variable esta definida o no*/
+$mexflix= new Router($route_index);
